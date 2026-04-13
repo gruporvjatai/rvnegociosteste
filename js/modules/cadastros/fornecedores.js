8cc31c08c6b4e3029a642407d4b7bf1520d8c845
Grupo RV <rvportalmadeiras@gmail.com>
2026-04-13 13:21:08 -0300

    Create fornecedores.js

---
+++ js/modules/cadastros/fornecedores.js
@@ -0,0 +1,6 @@
+async function initFornecedores() {
+    console.log("Aba Fornecedores carregada!");
+    await carregarFornecedores();
+}
+
+// COLE AQUI SUAS FUNÇÕES DE SUPABASE: carregarFornecedores(), salvarFornecedor(), excluirFornecedor(), etc.
