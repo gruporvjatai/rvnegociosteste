f64a1dacf140fa5a645396e405e2ce1661af11de
Grupo RV <rvportalmadeiras@gmail.com>
2026-04-13 13:42:54 -0300

    Delete js/modules directory

--- js/modules/cadastros/fornecedores.js
+++
@@ -1,6 +0,0 @@
-async function initFornecedores() {
-    console.log("Aba Fornecedores carregada!");
-    await carregarFornecedores();
-}
-
-// COLE AQUI SUAS FUNÇÕES DE SUPABASE: carregarFornecedores(), salvarFornecedor(), excluirFornecedor(), etc.
